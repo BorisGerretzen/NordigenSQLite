02d71e9ae104bfb5c86995f221b5efbbdd19c156
Boris Gerretzen <borisgerretzen@gmail.com>
2022-12-01 01:14:16 +0100

    Removed old project

--- NordigenLib/Models/API/Responses/TransactionsResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace NordigenLib.Models.API.Responses;
-
-[JsonObject]
-public class TransactionsResponse {
-    [JsonProperty("transactions")] public required Transactions Transactions { get; set; }
-}
--- NordigenLib/Models/API/Transactions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace NordigenLib.Models.API;
-
-[JsonObject]
-public class Transactions {
-    [JsonProperty("booked")] public List<Transaction> Booked { get; set; } = new();
-    [JsonProperty("pending")] public List<Transaction> Pending { get; set; } = new();
-}
